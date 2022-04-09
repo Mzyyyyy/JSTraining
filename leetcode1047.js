197970c1c781b1735bf37da8a5df6335a8bcca6f
‘xiaoma’ <mazhenyyyyy@163.com>
2022-04-09 22:17:37 +0800

    leetcode1047

---
+++ leetcode1047.js
@@ -0,0 +1,11 @@
+var removeDuplicates = function(s) {
+    const stk = [];
+    for (const ch of s) {
+        if (stk.length && stk[stk.length - 1] === ch) {
+            stk.pop();
+        } else {
+            stk.push(ch);
+        }
+    }
+    return stk.join('');
+};
