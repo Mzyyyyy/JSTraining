d903c3cfa88e19c8d3ad3aac4782099c1124d576
不愿透露姓名的小马 <Mazhenyyyyy@163.com>
2022-08-26 22:44:49 +0800

    704

---
+++ leetcode704.js
@@ -0,0 +1,16 @@
+
+var search = function(nums, target) {
+    let left = 0, right = nums.length - 1
+    while (left <= right) {
+        let mid = Math.floor((right - left) / 2) + left
+        let num = nums[mid]
+        if (num === target) {
+            return mid
+        } else if (num > target) {
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+    }
+    return -1
+}
